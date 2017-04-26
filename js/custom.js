91b8cddd0a661d88bf853e7067e78e336b982629
seanfagan <Fagan@smhfbp.local>
2017-04-25 22:29:38 -0500

    Overhaul
    Taking the time to do this site right.

--- js/custom.js
+++ js/custom.js
@@ -1,74 +1,20 @@
-var $root = $('html, body');
-$('a.project').click(function() {
-  var href = $.attr(this, 'href'),
-      $myProject = $(href),
-      $projects = $('.showcase');
+window.onload = function(){
+  setupSoundCloudEmbeds();
+}
 
-  // Animate scrolling to showcase
-  $root.animate({
-      scrollTop: $('#project-showcase').offset().top
-  }, 200);
+function hoverOnAbout() {
+  $("#portrait").attr('src', 'img/fagan-portrait-color.jpg');
+}
 
-  // Hide other projects, reveal chosen project
-  $projects.not($myProject).addClass("hidden");
-  $myProject.removeClass("hidden");
-  return false;
-});
+function hoverOffAbout() {
+  $("#portrait").attr('src', 'img/fagan-portrait-bw.jpg');
+}
 
-var shuffleme = (function( $ ) {
-  'use strict';
-  var $grid = $('#grid'), //locate what we want to sort 
-      $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
-      $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
-
-  init = function() {
-
-    // None of these need to be executed synchronously
-    setTimeout(function() {
-      listen();
-      setupFilters();
-    }, 100);
-
-    // instantiate the plugin
-    $grid.shuffle({
-      itemSelector: '[class*="col-"]',
-      sizer: $sizer    
-    });
-  },
-
-  setupFilters = function() {
-    var $btns = $filterOptions.children();
-    $btns.on('click', function(e) {
-      e.preventDefault();
-      var $this = $(this),
-          isActive = $this.hasClass( 'active' ),
-          group = isActive ? 'all' : $this.data('group');
-
-      // Change which link is active
-      if ( !isActive ) {
-        $('.portfolio-sorting li a').removeClass('active');
-      }
-      $this.toggleClass('active');
-
-      // Filter elements
-      $grid.shuffle( 'shuffle', group );
-    });
-
-    $btns = null;
-  },
-
-  listen = function() {
-    var debouncedLayout = $.throttle( 300, function() {
-      $grid.shuffle('update');
-    });
-  };
-
-  return {
-    init: init
-  };
-}( jQuery ));
-
-$(document).ready(function()
-{
-  shuffleme.init(); //filter portfolio
-});
+function setupSoundCloudEmbeds() {
+  var iframes = $("iframe.soundcloud").get();
+  $.each(iframes, function(index, iframe) {
+    iframe.setAttribute("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + iframe.dataset.track + "&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=false&amp;color=333333&amp;liking=false&amp;hide_cookies=true&amp;download=false");
+    iframe.setAttribute("scrolling", "no");
+    iframe.setAttribute("frameborder", "no");
+  });
+}
