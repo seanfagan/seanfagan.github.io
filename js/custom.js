d3ba985a570e27cb02e3773e0cc3390cbacf2436
seanfagan <seanfagandesign@gmail.com>
2017-10-17 23:14:35 -0500

    Release 1.0
    * Initial setup
    
    Simple bootstrap webpage
    
    * Drafted sections for bio and work experience
    
    * Built out "experience" section
    
    * Cleaned "experience" section
    
    * Changed styling of "experience" dropdowns
    
    * Update contents of "experience"
    
    * Prototyped "projects" showcase
    
    * Separated experience sections into separate rows
    
    * Removed bootstrap-theme.min.css
    
    * Cleanup for responsiveness
    
    * Resolving conflicts part 2
    
    * Add graphics section
    
    * More showcase images
    
    * More showcase images
    
    * Cleanup
    
    * Finalizing initial development!

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+var year = new Date().getFullYear();
+document.getElementById("copyright").textContent = year;
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+
+$('#portrait').hover(function() {
+  $(this).attr('src', 'img/fagan-portrait-color.jpg');
+}, function() {
+  $(this).attr('src', 'img/fagan-portrait-bw.jpg');
+});
+
+
+$(document).on('click', 'a.soundcloud', function(e) {
+  var $link = $(e.target);
+  var track = $link.data("track");
+  $(e.target).replaceWith('<iframe width="100%" height="115" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track + '&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=false&amp;color=333333&amp;liking=false&amp;hide_cookies=true&amp;download=false"></iframe>');
+});
