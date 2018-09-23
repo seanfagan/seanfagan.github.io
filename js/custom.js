7c02099944cd806ee626274e11b92fe4cac66f3a
Sean Fagan <seanfagandesign@gmail.com>
2018-09-23 03:31:44 +0000

    Remove unused scripts

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,10 @@
-var year = new Date().getFullYear();
-document.getElementById("copyright").textContent = year;
-
+/* Lightbox */
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
 
-
+/* Grayscale animation */
 $(window).scroll(function() {
   var windowH = $(window).height();
   var scrollY = $(window).scrollTop();
@@ -26,12 +24,7 @@ $(window).scroll(function() {
   }
 });
 
+/* Back to top animation */
 $("#toTop").click(function () {
    $("html, body").animate({scrollTop: 0}, 1000);
-});
-
-$(document).on('click', 'a.soundcloud', function(e) {
-  var $link = $(e.target);
-  var track = $link.data("track");
-  $(e.target).replaceWith('<iframe width="100%" height="115" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track + '&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=false&amp;color=333333&amp;liking=false&amp;hide_cookies=true&amp;download=false"></iframe>');
 });
