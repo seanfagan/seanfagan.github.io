928768e926b8bbd53afcbf0bb36a3179d1aa07ab
Sean H Fagan <seanfagandesign@gmail.com>
2020-01-06 17:17:44 -0600

    Improve grayscale animation

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,19 @@
+window.addEventListener('load', (event) => {
+  /* Grayscale animation */
+  $('.grayscale').addClass("ungrayscale");
+});
+
 /* Lightbox */
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
 
-/* Grayscale animation */
 $(window).scroll(function() {
-  var windowH = $(window).height();
+  /* Header scrolling fade */
   var scrollY = $(window).scrollTop();
   
-  $('.grayscale').each(function() {
-    var imgPosition = $(this).offset().top;
-    if (scrollY > imgPosition - windowH) {
-      $(this).addClass("ungrayscale");
-    }
-  });
-  
-  var alpha = .5 - scrollY * 0.001
+  var alpha = .5 - scrollY * 0.002;
   if (alpha > 0) {
   	$("#header .row").css({
       "background": "linear-gradient(to bottom right, rgba(45,0,5," + alpha + "), rgba(35,5,60," + alpha + "))",
